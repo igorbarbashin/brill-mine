e1223161014286bb9c2003ca737ab2948f49c554
Igor Barbashin <igor.barbashin@gmail.com>
2020-04-24 09:21:57 -0700

    Hardcode tokenURI construction

--- contracts/Brill.sol
+++ contracts/Brill.sol
@@ -1,24 +1,29 @@
 pragma solidity 0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./Util.sol";
 
 
-contract Brill is ERC721 {
+contract Brill is Util, ERC721 {
     using Counters for Counters.Counter;
+    using Strings for uint256;
     Counters.Counter private _tokenIds;
+
+    // Add constant address of the DAO contract
+    // Make mint function payable
+    // Hardcorde the prefix of the url
+    string constant VIEW_URI_BASE = "https://brill.diamonds/d";
 
     constructor() public ERC721("Brill", "BRILL") {}
 
     // tokenURI: https://brill.diamonds/d/1
-    function mint(address minter, string memory tokenURI)
-        public
-        returns (uint256)
-    {
+    function mint(address minter) public returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(minter, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, strConcat(VIEW_URI_BASE, newItemId.toString()));
 
         return newItemId;
     }
---
+++ contracts/Util.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.6.6;
+
+
+contract Util {
+    /**
+     * @notice Concatenate two strings
+     * @param _a the first string
+     * @param _b the second string
+     * @return result the concatenation of `_a` and `_b`
+     */
+    function strConcat(string memory _a, string memory _b)
+        internal
+        pure
+        returns (string memory result)
+    {
+        result = string(abi.encodePacked(bytes(_a), bytes(_b)));
+    }
+}
