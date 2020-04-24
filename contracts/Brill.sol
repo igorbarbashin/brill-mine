ef717ced55d4273afa3fe973c6c891d2134777b0
Igor Barbashin <igor.barbashin@gmail.com>
2020-04-24 07:34:18 -0700

    Build basic ERC721 token

--- contracts/Brill.sol
+++ contracts/Brill.sol
@@ -1,8 +1,25 @@
-pragma solidity 0.6.4;
-import "@openzeppelin/token/ERC721/ERC721.sol";
-import "@openzeppelin/token/ERC721/ERC721Metadata.sol";
+pragma solidity 0.6.6;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract Brill is ERC721, ERC721Metadata {
-    constructor() public ERC721Metadata("Brill", "BRILL") {}
+contract Brill is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("Brill", "BRILL") {}
+
+    // tokenURI: https://brill.diamonds/d/1
+    function mint(address minter, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(minter, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
 }
